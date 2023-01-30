bd671e1f2f75221d970610eea0abf0a3050960e2
yousrah24 <souleyousrah@gmail.com>
2023-01-30 20:17:31 +0100

    modification des procedure et ajout du database

--- SQL_SAE_Tourisme/CreationAlimentsGroupesSante_InsertionDeDonnees.sql
+++ SQL_SAE_Tourisme/CreationAlimentsGroupesSante_InsertionDeDonnees.sql
@@ -1,3 +1,12 @@
+CREATE DATABASE IF NOT EXISTS  sondage;
+USE sondage;
+
+DROP TABLE  IF EXISTS ALIMENTS;
+DROP TABLE  IF EXISTS GROUPE_ALIMENTS  ;
+DROP TABLE  IF EXISTS SS_GROUPE_ALIMENTS  ;
+DROP TABLE  IF EXISTS SSSS_GROUPE_ALIMENTS  ;
+DROP TABLE  IF EXISTS DONNEES_SANTE   ;
+
 CREATE TABLE ALIMENTS (
 	Alim_Code INTEGER,
 	Nom_FR VARCHAR(300),
@@ -25,70 +34,70 @@ CREATE TABLE SSSS_GROUPE_ALIMENTS (
 
 CREATE TABLE DONNEES_SANTE (
 	Alim_Code INTEGER,
-	Energie_Reglement_UE_kJ100g NUMBER(6, 2),
-    Energie_Reglement_UE_kcal100g NUMBER(6, 3),
-    Energie_NxfacteurJones_kJ100g NUMBER(6, 2),
-    Energie_NxfacteurJones_kca100g NUMBER(6, 3),
-    Eau_g100g NUMBER(6, 3),
-    Proteines_NxfacteurJones_g100g NUMBER(6, 4),
-    Proteines_Nx625_g100g NUMBER(6, 4),
-    Glucides_g100g NUMBER(7, 5),
-    Lipides_g100g NUMBER(7, 4),
-    Sucres_g100g NUMBER(6, 4),
-    Fructose_g100g NUMBER(6, 4),
-    Galactose_g100g NUMBER(5, 4),
-    Glucose_g100g NUMBER(6, 4),
-    Lactose_g100g NUMBER(6, 4),
-    Maltose_g100g NUMBER(6, 4),
-    Saccharose_g100g NUMBER(7, 5),
-    Amidon_g100g NUMBER(7, 5),
-    Fibres_alimentaires_g100g NUMBER(7, 5),
-    Polyols_totaux_g100g NUMBER(7, 5),
-    Cendres_g100g NUMBER(8, 6),
-    Alcool_g100g NUMBER(6, 4),
-    Acides_organiques_g100g NUMBER(7, 6),
-    AG_satures_g100g NUMBER(7, 5),
-    AG_monoinsatures_g100g NUMBER(7, 5),
-    AG_polyinsatures_g100g NUMBER(7, 5),
-    AG_butyrique_g100g NUMBER(7, 6),
-    AG_capro_que_g100g NUMBER(6, 5),
-    AG_caprylique_g100g NUMBER(6, 5),
-    AG_caprique_g100g NUMBER(7, 6),
-    AG_laurique_g100g NUMBER(7, 5),
-    AG_myristique_g100g NUMBER(7, 5),
-    AG_palmitique_g100g NUMBER(7, 5),
-    AG_stearique_g100g NUMBER(7, 5),
-    AG_oleique_g100g NUMBER(7, 5),
-    AG_linoleique_g100g NUMBER(7, 5),
-    AG_alphanulllinolenique_g100g NUMBER(7, 5),
-    AG_arachidonique_g100g NUMBER(7, 6),
-    AG_EPA_g100g NUMBER(8, 6),
-    AG_DHA_g100g NUMBER(8, 6),
-    Cholesterol_mg100g NUMBER(8, 4),
-    Sel_chlorure_de_sodium_g100g NUMBER(8, 6),
-    Calcium_mg100g NUMBER(7, 3),
-    Chlorure_mg100g NUMBER(7, 2),
-    Cuivre_mg100g NUMBER(7, 5),
-    Fer_mg100g NUMBER(8, 5),
-    Iode_microg100g NUMBER(9, 3),
-    Magnesium_mg100g NUMBER(7, 3),
-    Manganese_mg100g NUMBER(8, 5),
-    Phosphore_mg100g NUMBER(8, 4),
-    Potassium_mg100g NUMBER(8, 3),
-    Selenium_microg100g NUMBER(6, 3),
-    Sodium_mg100g NUMBER(7, 2),
-    Zinc_mg100g NUMBER(6, 4),
-    Retinol_microg100g NUMBER(8, 3),
-    BetaCarotene_microg100g NUMBER(11, 5),
-    Vitamine_D_microg100g NUMBER(8, 5),
-    Vitamine_E_mg100g NUMBER(7, 4),
-    Vitamine_K1_microg100g NUMBER(7, 3),
-    Vitamine_K2_microg100g NUMBER(5, 2),
-    Vitamine_C_mg100g NUMBER(8, 4),
-    Vitamine_B1_Thia_mg100g NUMBER(7, 5),
-    Vitamine_B2_Ribo_mg100g NUMBER(5, 4),
-    Vitamine_B3_PP_Nia_mg100g NUMBER(7, 4),
-    Vitamine_B5_AcidePan_mg100g NUMBER(7, 4)
+	Energie_Reglement_UE_kJ100g DECIMAL(6, 2),
+    Energie_Reglement_UE_kcal100g DECIMAL(6, 3),
+    Energie_NxfacteurJones_kJ100g DECIMAL(6, 2),
+    Energie_NxfacteurJones_kca100g DECIMAL(6, 3),
+    Eau_g100g DECIMAL(6, 3),
+    Proteines_NxfacteurJones_g100g DECIMAL(6, 4),
+    Proteines_Nx625_g100g DECIMAL(6, 4),
+    Glucides_g100g DECIMAL(7, 5),
+    Lipides_g100g DECIMAL(7, 4),
+    Sucres_g100g DECIMAL(6, 4),
+    Fructose_g100g DECIMAL(6, 4),
+    Galactose_g100g DECIMAL(5, 4),
+    Glucose_g100g DECIMAL(6, 4),
+    Lactose_g100g DECIMAL(6, 4),
+    Maltose_g100g DECIMAL(6, 4),
+    Saccharose_g100g DECIMAL(7, 5),
+    Amidon_g100g DECIMAL(7, 5),
+    Fibres_alimentaires_g100g DECIMAL(7, 5),
+    Polyols_totaux_g100g DECIMAL(7, 5),
+    Cendres_g100g DECIMAL(8, 6),
+    Alcool_g100g DECIMAL(6, 4),
+    Acides_organiques_g100g DECIMAL(7, 6),
+    AG_satures_g100g DECIMAL(7, 5),
+    AG_monoinsatures_g100g DECIMAL(7, 5),
+    AG_polyinsatures_g100g DECIMAL(7, 5),
+    AG_butyrique_g100g DECIMAL(7, 6),
+    AG_capro_que_g100g DECIMAL(6, 5),
+    AG_caprylique_g100g DECIMAL(6, 5),
+    AG_caprique_g100g DECIMAL(7, 6),
+    AG_laurique_g100g DECIMAL(7, 5),
+    AG_myristique_g100g DECIMAL(7, 5),
+    AG_palmitique_g100g DECIMAL(7, 5),
+    AG_stearique_g100g DECIMAL(7, 5),
+    AG_oleique_g100g DECIMAL(7, 5),
+    AG_linoleique_g100g DECIMAL(7, 5),
+    AG_alphanulllinolenique_g100g DECIMAL(7, 5),
+    AG_arachidonique_g100g DECIMAL(7, 6),
+    AG_EPA_g100g DECIMAL(8, 6),
+    AG_DHA_g100g DECIMAL(8, 6),
+    Cholesterol_mg100g DECIMAL(8, 4),
+    Sel_chlorure_de_sodium_g100g DECIMAL(8, 6),
+    Calcium_mg100g DECIMAL(7, 3),
+    Chlorure_mg100g DECIMAL(7, 2),
+    Cuivre_mg100g DECIMAL(7, 5),
+    Fer_mg100g DECIMAL(8, 5),
+    Iode_microg100g DECIMAL(9, 3),
+    Magnesium_mg100g DECIMAL(7, 3),
+    Manganese_mg100g DECIMAL(8, 5),
+    Phosphore_mg100g DECIMAL(8, 4),
+    Potassium_mg100g DECIMAL(8, 3),
+    Selenium_microg100g DECIMAL(6, 3),
+    Sodium_mg100g DECIMAL(7, 2),
+    Zinc_mg100g DECIMAL(6, 4),
+    Retinol_microg100g DECIMAL(8, 3),
+    BetaCarotene_microg100g DECIMAL(11, 5),
+    Vitamine_D_microg100g DECIMAL(8, 5),
+    Vitamine_E_mg100g DECIMAL(7, 4),
+    Vitamine_K1_microg100g DECIMAL(7, 3),
+    Vitamine_K2_microg100g DECIMAL(5, 2),
+    Vitamine_C_mg100g DECIMAL(8, 4),
+    Vitamine_B1_Thia_mg100g DECIMAL(7, 5),
+    Vitamine_B2_Ribo_mg100g DECIMAL(5, 4),
+    Vitamine_B3_PP_Nia_mg100g DECIMAL(7, 4),
+    Vitamine_B5_AcidePan_mg100g DECIMAL(7, 4)
 );
 
 ALTER TABLE ALIMENTS ADD CONSTRAINT PK_ALIMENTS PRIMARY KEY(Alim_code);
@@ -97,11 +106,11 @@ ALTER TABLE SS_GROUPE_ALIMENTS ADD CONSTRAINT PK_SS_GROUPE_ALIMENTS PRIMARY KEY(
 ALTER TABLE SSSS_GROUPE_ALIMENTS ADD CONSTRAINT PK_SSSS_GROUPE_ALIMENTS PRIMARY KEY(SSSS_Grp_code);
 ALTER TABLE DONNEES_SANTE ADD CONSTRAINT PK_DONNEES_SANTE PRIMARY KEY(Alim_Code);
 
-ALTER TABLE ALIMENTS ADD CONSTRAINT FK_ALIMENTS_GROUPE FOREIGN KEY(Grp_Code) REFERENCES GROUPE_ALIMENTS(Grp_Code);
-ALTER TABLE ALIMENTS ADD CONSTRAINT FK_ALIMENTS_SS_GROUPE FOREIGN KEY(SS_Grp_Code) REFERENCES SS_GROUPE_ALIMENTS(SS_Grp_Code);
-ALTER TABLE ALIMENTS ADD CONSTRAINT FK_ALIMENTS_SSSS_GROUPE FOREIGN KEY(SSSS_Grp_Code) REFERENCES SSSS_GROUPE_ALIMENTS(SSSS_Grp_Code);
+ALTER TABLE ALIMENTS ADD CONSTRAINT FK_ALIMENTS_GROUPE FOREIGN KEY(Grp_Code) REFERENCES GROUPE_ALIMENTS(Grp_Code) ON DELETE CASCADE;
+ALTER TABLE ALIMENTS ADD CONSTRAINT FK_ALIMENTS_SS_GROUPE FOREIGN KEY(SS_Grp_Code) REFERENCES SS_GROUPE_ALIMENTS(SS_Grp_Code) ON DELETE CASCADE;
+ALTER TABLE ALIMENTS ADD CONSTRAINT FK_ALIMENTS_SSSS_GROUPE FOREIGN KEY(SSSS_Grp_Code) REFERENCES SSSS_GROUPE_ALIMENTS(SSSS_Grp_Code) ON DELETE CASCADE;
 
-ALTER TABLE DONNEES_SANTE ADD CONSTRAINT FK_DONNEES_SANTE_ALIMENTS FOREIGN KEY(Alim_Code) REFERENCES ALIMENTS(Alim_Code);
+ALTER TABLE DONNEES_SANTE ADD CONSTRAINT FK_DONNEES_SANTE_ALIMENTS FOREIGN KEY(Alim_Code) REFERENCES ALIMENTS(Alim_Code) ON DELETE CASCADE;
 
 INSERT INTO GROUPE_ALIMENTS (Grp_Code, Nom_Fr) SELECT DISTINCT alim_grp_code, alim_grp_nom_fr FROM ALIMENTSBRUT;
 INSERT INTO SS_GROUPE_ALIMENTS (SS_Grp_Code, Nom_Fr) SELECT DISTINCT alim_ssgrp_code, alim_ssgrp_nom_fr FROM ALIMENTSBRUT;
