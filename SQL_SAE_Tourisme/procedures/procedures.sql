6fd4005a2952bd02e61f05a9d475bd92806990b1
Adrien Deü <75019689+AdrienDeu@users.noreply.github.com>
2023-02-02 15:13:55 +0100

    Modifs

--- SQL_SAE_Tourisme/procedures/procedures.sql
+++ SQL_SAE_Tourisme/procedures/procedures.sql
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-DROP TABLE IF EXISTS Client;
-
-CREATE TABLE Client(
-   idClient INT AUTO_INCREMENT,
-   nom VARCHAR(50) NOT NULL,
-   email VARCHAR(50) NOT NULL,
-   mdp VARCHAR(50) NOT NULL,
-   CONSTRAINT PK_Client PRIMARY KEY(idClient),
-   CONSTRAINT U_PseudoEmail UNIQUE(nom, email)
-);
-
 DROP PROCEDURE IF EXISTS addClient;
 DROP FUNCTION IF EXISTS existeUtilisateur;
 DROP PROCEDURE IF EXISTS deleteClient;
 
 delimiter $
-CREATE PROCEDURE addClient(nom_ VARCHAR(50), mdp_ VARCHAR(50), email_ VARCHAR(50))
+CREATE PROCEDURE addClient(Nom_ VARCHAR(50), Prenom_ VARCHAR(50), Naissance_ VARCHAR(10), mdp_ VARCHAR(50), email_ VARCHAR(50), Adresse_ VARCHAR(100), CodePostal_ CHAR(6), Ville_ VARCHAR(100), Telephone_ CHAR(14))
 BEGIN
-   INSERT INTO Client (idClient, nom, mdp) VALUES (NULL, nom_, mdp_, email_);
+   INSERT INTO utilisateur (Administre, Nom, Prenom, Naissance, Adresse, CodePostal, Ville, Telephone, mdp) VALUES (NULL, Nom_, Prenom_, Naissance_, Adresse_, CodePostal_, Ville_, Telephone_, mdp_);
 END $
 
 delimiter $
@@ -41,47 +29,3 @@ END $$
 
 
 
-=======
-DROP TABLE IF EXISTS Client;
-
-CREATE TABLE Client(
-   idClient INT AUTO_INCREMENT,
-   nom VARCHAR(50) NOT NULL,
-   email VARCHAR(50) NOT NULL,
-   mdp VARCHAR(50) NOT NULL,
-   CONSTRAINT PK_Client PRIMARY KEY(idClient),
-   CONSTRAINT U_PseudoEmail UNIQUE(nom, email)
-);
-
-DROP PROCEDURE IF EXISTS addClient;
-DROP FUNCTION IF EXISTS existeUtilisateur;
-DROP PROCEDURE IF EXISTS deleteClient;
-
-delimiter $
-CREATE PROCEDURE addClient(nom_ VARCHAR(50), mdp_ VARCHAR(50), email_ VARCHAR(50))
-BEGIN
-   INSERT INTO Client (idClient, nom, mdp) VALUES (NULL, nom_, mdp_, email_);
-END $
-
-delimiter $
-CREATE PROCEDURE deleteClient(nom_ VARCHAR(50), mdp_ VARCHAR(50), email_ VARCHAR(50))
-BEGIN
-    if ((SELECT mdp FROM Client C WHERE (C.nom=nom_ and C.email=email_))=mdp_) THEN
-    DELETE FROM Client WHERE (nom_= Client.nom and email_=Client.email);
-    END IF;
-END $
-
-
-
-delimiter $$
-CREATE FUNCTION existeUtilisateur(nom_ VARCHAR(50), email_ VARCHAR(50)) RETURNS BOOLEAN
-BEGIN
-   if ((SELECT COUNT(*) FROM Client C WHERE (C.nom=nom_ and C.email=email_))<>0) THEN
-    RETURN TRUE;
-    END IF;
-    RETURN FALSE;
-END $$
-
-
-
->>>>>>> c85260d3a9928a4a9a0afa6552115170af8f3eeb
