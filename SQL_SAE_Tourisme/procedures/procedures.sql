c3eb362943f58d849ffb39629c9a601f5c672e1d
Adrien Deü <75019689+AdrienDeu@users.noreply.github.com>
2023-02-02 15:17:17 +0100

    Re modifs mineurs

--- SQL_SAE_Tourisme/procedures/procedures.sql
+++ SQL_SAE_Tourisme/procedures/procedures.sql
@@ -1,18 +1,18 @@
-DROP PROCEDURE IF EXISTS addClient;
+DROP PROCEDURE IF EXISTS addUtilisateur;
 DROP FUNCTION IF EXISTS existeUtilisateur;
-DROP PROCEDURE IF EXISTS deleteClient;
+DROP PROCEDURE IF EXISTS deleteUtilisateur;
 
 delimiter $
-CREATE PROCEDURE addClient(Nom_ VARCHAR(50), Prenom_ VARCHAR(50), Naissance_ VARCHAR(10), mdp_ VARCHAR(50), email_ VARCHAR(50), Adresse_ VARCHAR(100), CodePostal_ CHAR(6), Ville_ VARCHAR(100), Telephone_ CHAR(14))
+CREATE PROCEDURE addUtilisateur(Nom_ VARCHAR(50), Prenom_ VARCHAR(50), Naissance_ VARCHAR(10), mdp_ VARCHAR(50), email_ VARCHAR(50), Adresse_ VARCHAR(100), CodePostal_ CHAR(6), Ville_ VARCHAR(100), Telephone_ CHAR(14))
 BEGIN
    INSERT INTO utilisateur (Administre, Nom, Prenom, Naissance, Adresse, CodePostal, Ville, Telephone, mdp) VALUES (NULL, Nom_, Prenom_, Naissance_, Adresse_, CodePostal_, Ville_, Telephone_, mdp_);
 END $
 
 delimiter $
-CREATE PROCEDURE deleteClient(nom_ VARCHAR(50), mdp_ VARCHAR(50), email_ VARCHAR(50))
+CREATE PROCEDURE deleteUtilisateur(nom_ VARCHAR(50), mdp_ VARCHAR(50), email_ VARCHAR(50))
 BEGIN
-    if ((SELECT mdp FROM Client C WHERE (C.nom=nom_ and C.email=email_))=mdp_) THEN
-    DELETE FROM Client WHERE (nom_= Client.nom and email_=Client.email);
+    if ((SELECT mdp FROM Utilisateur C WHERE (C.nom=nom_ and C.email=email_))=mdp_) THEN
+    DELETE FROM Utilisateur WHERE (nom_= Utilisateur.nom and email_=Utilisateur.email);
     END IF;
 END $
 
@@ -21,7 +21,7 @@ END $
 delimiter $$
 CREATE FUNCTION existeUtilisateur(nom_ VARCHAR(50), email_ VARCHAR(50)) RETURNS BOOLEAN
 BEGIN
-   if ((SELECT COUNT(*) FROM Client C WHERE (C.nom=nom_ and C.email=email_))<>0) THEN
+   if ((SELECT COUNT(*) FROM Utilisateur C WHERE (C.nom=nom_ and C.email=email_))<>0) THEN
     RETURN TRUE;
     END IF;
     RETURN FALSE;
