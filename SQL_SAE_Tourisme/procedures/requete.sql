4adb3382259938e5ddcd4568426ac9adac4ed563
Adrien Deü <deu.adrien@gmail.com>
2023-02-06 15:34:13 +0100

    correction bug

--- SQL_SAE_Tourisme/procedures/requete.sql
+++ SQL_SAE_Tourisme/procedures/requete.sql
@@ -1,4 +1,7 @@
+/* Les 10 aliments les plus chosis. */
 SELECT S.Alim_Code, AL.Nom_Fr, count(S.Alim_Code)
 FROM REPONSE_SONDAGE S JOIN  ALIMENTS AL ON S.Alim_Code = AL.Alim_Code 
 GROUP BY S.Alim_Code, AL.Nom_Fr
 ORDER BY 3 DESC;
+
+ /* Le score santé */
