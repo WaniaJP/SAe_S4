01b8813b06ed4a55f0240f131d61ed34e7dbbf44
Adrien Deü <75019689+AdrienDeu@users.noreply.github.com>
2023-03-08 12:18:58 +0100

    Add files via upload

--- SQL_SAE_Tourisme/procedures/procedures.sql
+++ SQL_SAE_Tourisme/procedures/procedures.sql
@@ -3,9 +3,9 @@ DROP FUNCTION IF EXISTS existeUtilisateur;
 DROP PROCEDURE IF EXISTS deleteUtilisateur;
 
 delimiter $
-CREATE PROCEDURE addUtilisateur(Nom_ VARCHAR(50), Prenom_ VARCHAR(50), Naissance_ VARCHAR(10), mdp_ VARCHAR(50), email_ VARCHAR(50), Adresse_ VARCHAR(100), CodePostal_ CHAR(6), Ville_ VARCHAR(100), Telephone_ CHAR(14))
+CREATE PROCEDURE addUtilisateur(Nom_ VARCHAR(50), Prenom_ VARCHAR(50), Naissance_ VARCHAR(10), mdp_ VARCHAR(50), email_ VARCHAR(50), Ville_ VARCHAR(100), Telephone_ CHAR(14))
 BEGIN
-   INSERT INTO utilisateur (Administre, Nom, Prenom, Naissance, Adresse, CodePostal, Ville, Telephone, mdp) VALUES (NULL, Nom_, Prenom_, Naissance_, Adresse_, CodePostal_, Ville_, Telephone_, mdp_);
+   INSERT INTO utilisateur (Administre, Nom, Prenom, Naissance,  Ville, Telephone, mdp) VALUES (NULL, Nom_, Prenom_, Naissance_, Ville_, Telephone_, mdp_);
 END $
 
 delimiter $
